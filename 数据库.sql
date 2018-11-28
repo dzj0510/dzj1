7195c5958f8de25445eb2be2b608cae3eb91efc7
dzj0510 <40760481+dzj0510@users.noreply.github.com>
2018-11-28 14:46:01 +0800

    Add files via upload

--- 数据库.sql
+++ 数据库.sql
@@ -6,23 +6,24 @@ use python_test;
 select database()
 
 
---students表
+--students表gender enum('男','女','中性','保密') default '保密',
 create table students(
 	id int unsigned primary key auto_increment not null,
 	name varchar(20) default '',
-	age tinyint unsinged default 0,
+	age tinyint unsigned default 0,
 	height decimal(5,2),
 	gender enum('男','女','中性','保密') default '保密',
-	cls_id int unsinged default 0,
+	cls_id int unsigned default 0,
 	is_delete bit default 0
 );
 
 --classes表
-create talbe classes(
-	id int unsinged auto_increment primary key not null,
+create table classes(
+	id int unsigned auto_increment primary key not null,
 	name varchar(30) not null
 );
 
+--dengzejun
 
 
 
