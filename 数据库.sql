80440315f618971488783b6e90e3e8b6df897ac9
dzj0510 <40760481+dzj0510@users.noreply.github.com>
2018-11-28 14:43:48 +0800

    Add files via upload

---
+++ 数据库.sql
@@ -0,0 +1,28 @@
+--创建数据库
+create database python_test charset=utf8;
+--使用数据库
+use python_test;
+--显示使用的当前数据是哪个
+select database()
+
+
+--students表
+create table students(
+	id int unsigned primary key auto_increment not null,
+	name varchar(20) default '',
+	age tinyint unsinged default 0,
+	height decimal(5,2),
+	gender enum('男','女','中性','保密') default '保密',
+	cls_id int unsinged default 0,
+	is_delete bit default 0
+);
+
+--classes表
+create talbe classes(
+	id int unsinged auto_increment primary key not null,
+	name varchar(30) not null
+);
+
+
+
+
